918f6cba5694dee7ec5fd871215bc2ba36c82ecf
Eric Searcy <emsearcy@gmail.com>
2015-04-20 09:44:46 -0700

    Accomodate bconsole output changes (case sensitivity)

--- wormvolmgmt.py
+++ wormvolmgmt.py
@@ -34,35 +34,35 @@ def main():
          if (
                # Statuses that mean we won't write again
                (
-                  vol['VolStatus'] == 'Used' or
-                  vol['VolStatus'] == 'Read-Only' or
+                  vol['volstatus'] == 'Used' or
+                  vol['volstatus'] == 'Read-Only' or
                   (
                      # Unwritten "Error" can be left writeable for purge
-                     vol['VolStatus'] == 'Error' and
-                     vol['LastWritten'] != None
+                     vol['volstatus'] == 'Error' and
+                     vol['lastwritten'] != None
                   )
                ) and
                # We will be able to chmod the file
-               os.access(device + vol['VolumeName'], os.W_OK)
+               os.access(device + vol['volumename'], os.W_OK)
             ):
-            print "[%s] Committing %s volume %s to WORM..." % (datetime.now(), vol['VolStatus'], vol['VolumeName'])
+            print "[%s] Committing %s volume %s to WORM..." % (datetime.now(), vol['volstatus'], vol['volumename'])
             committoworm(vol, device)
 
          # Delete past-retention volumes
          if (
                # Statuses to delete
-               vol['VolStatus'] == 'Purged' or
+               vol['volstatus'] == 'Purged' or
                (
-                  vol['VolStatus'] == 'Error' and
+                  vol['volstatus'] == 'Error' and
                   (
                      # "Error" volumes won't be auto-purged, so we can estimate
                      # retention time, unless it was never written to
-                     vol['LastWritten'] == None or
-                     vol['LastWritten'] + timedelta(seconds=vol['VolRetention']) < datetime.now()
+                     vol['lastwritten'] == None or
+                     vol['lastwritten'] + timedelta(seconds=vol['volretention']) < datetime.now()
                   )
                )
             ):
-            print "[%s] Destroying %s volume %s..." % (datetime.now(), vol['VolStatus'], vol['VolumeName'])
+            print "[%s] Destroying %s volume %s..." % (datetime.now(), vol['volstatus'], vol['volumename'])
             delwormfile(vol, device)
 
    except OSError as e:
@@ -76,12 +76,12 @@ def main():
       sys.stderr.write(e.output)
 
 def committoworm(volume, device):
-   if not (volume['VolStatus'] == 'Used' or volume['VolStatus'] == 'Read-Only' or volume['VolStatus'] == 'Error'):
+   if not (volume['volstatus'] == 'Used' or volume['volstatus'] == 'Read-Only' or volume['volstatus'] == 'Error'):
       # Ensure volume is in a state where it will not be written further
       newstate = 'Used'
 
       bc = subprocess.Popen(['/usr/sbin/bconsole', '-n'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
-      cmd = "update volume=%s VolStatus=%s" % (volume['VolumeName'], newstate)
+      cmd = "update volume=%s VolStatus=%s" % (volume['volumename'], newstate)
       (bc_out, bc_err) = bc.communicate(cmd)
 
       if bc.returncode != 0:
@@ -94,33 +94,33 @@ def committoworm(volume, device):
 
    # Generate NetApp SnapLock protection age to match Bacula retention
    epoch = datetime(1970, 1, 1)
-   atime_delta = volume['LastWritten'] + timedelta(seconds=volume['VolRetention']) - epoch
+   atime_delta = volume['lastwritten'] + timedelta(seconds=volume['volretention']) - epoch
    atime = atime_delta.days * 86400 + atime_delta.seconds
 
-   volfile = device + volume['VolumeName']
+   volfile = device + volume['volumename']
 
    try:
       # Commit to WORM by setting future access time, remove write perms
       os.utime(volfile, (atime, os.stat(volfile).st_mtime))
-      os.chmod(device + volume['VolumeName'], 0440)
+      os.chmod(device + volume['volumename'], 0440)
    except IOError as e:
-      print "[%s] Warning: WORM commit failed for volume %s" % (datetime.now(), volume['VolumeName'])
+      print "[%s] Warning: WORM commit failed for volume %s" % (datetime.now(), volume['volumename'])
 
 def delwormfile(volume, device):
-      volfile = device + volume['VolumeName']
+      volfile = device + volume['volumename']
       if not os.access(volfile, os.F_OK):
          # No file, just remove from Bacula
-         deletevol(volume['VolumeName'])
+         deletevol(volume['volumename'])
       else:
          volstat = os.stat(volfile)
          if datetime.fromtimestamp(volstat.st_atime) < datetime.now():
             # Remove from Bacula
-            deletevol(volume['VolumeName'])
+            deletevol(volume['volumename'])
             # Remove from file system
             os.unlink(volfile)
          else:
             # Don't try to remove a volume still protected by SnapLock
-            print "[%s] Warning: skipping future-atime volume %s" % (datetime.now(), volume['VolumeName'])
+            print "[%s] Warning: skipping future-atime volume %s" % (datetime.now(), volume['volumename'])
 
 def getmedia(pool):
    bc = subprocess.Popen(['/usr/sbin/bconsole', '-n'], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
@@ -130,26 +130,46 @@ def getmedia(pool):
    if bc.returncode != 0:
       raise BConsoleError("bconsole exited %s" % bc.returncode, cmd, bc_out)
 
+   # Attributes for each media instance are seperated by newlines, with two
+   # newlines seperating media instances from each other
    result = []
    i = 0
    for entry in bc_out.split("\n\n"):
-      for column in entry.splitlines():
-         if re.match('.*: ERR=', column):
-            raise BConsoleError('bconsole command error', cmd, column)
-         if len(result) == i+1 or column.strip()[:9] == 'MediaId: ':
-            key, value = column.strip().split(': ', 1)
+      for attribute in entry.splitlines():
+         if re.match('.*: ERR=', attribute):
+            raise BConsoleError('bconsole command error', cmd, attribute)
+
+         attribute = attribute.lstrip()
+         # Parse if we already started parsing or the key is mediaid
+         if len(result) == i+1 or attribute.lower()[:9] == 'mediaid: ':
+            # Parse the attribute name and value
+            sep = attribute.find(': ')
+            if sep == -1:
+               raise BConsoleError('bconsole parse error', cmd, attribute)
+            # Save the attribute name (array key) as lowercase for
+            # compatibility across Bacula versions
+            key = attribute[:sep].lower()
+            value = attribute[sep+2:]
+
+            # Add a result if this is a new mediaid
             if len(result) <= i:
                result.append({})
+
+            # Additional handling of special values
             if re.match('[0-9]{1,3}(,[0-9]{3})*\.[0-9]+$', value):
                value = locale.atof(value)
             elif re.match('[0-9]{1,3}(,[0-9]{3})*$', value):
                value = locale.atoi(value)
             elif value == '0000-00-00 00:00:00':
                value = None
+            elif value == '':
+               value = None
             elif re.match('[0-9]{4}-[0-9][0-9]-[0-9][0-9] [0-9][0-9]:[0-9][0-9]:[0-9][0-9]', value):
                value = datetime.strptime(value, '%Y-%m-%d %H:%M:%S')
 
+            # Save this attribute into this result
             result[i][key] = value
+      # Next result slot
       i += 1
 
    return result
